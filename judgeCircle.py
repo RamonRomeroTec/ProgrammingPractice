bd9580bf22c6dea7714a2a139125e7cd1e44bb51
RamonRomeroTec <A01700318@itesm.mx>
2018-08-31 16:27:02 -0500

    Create judgeCircle.py

---
+++ judgeCircle.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def judgeCircle(self, moves):
+        
+        horizontal=0
+        vertical=0
+        
+        
+        for i in range(0,len(moves)):
+            if moves[i]=='U':
+                vertical=vertical+1 
+            elif moves[i] == 'D':
+                vertical=vertical-1
+            elif moves[i]=='L':
+                horizontal=horizontal-1
+            elif moves[i]=='R':
+                horizontal=horizontal+1
+                
+        if (vertical==0 and horizontal==0):
+            return True
+        else:
+            return False
