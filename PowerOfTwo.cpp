39030981997517debebd36af8fa2591fa345ae7e
RamonRomeroTec <A01700318@itesm.mx>
2018-09-03 18:24:24 -0500

    Create PowerOfTwo.cpp

---
+++ PowerOfTwo.cpp
@@ -0,0 +1,22 @@
+// @AleLopez1312
+// @ramonromerotec
+
+class Solution {
+public:
+    /**
+     * @param n: an integer
+     * @return: if n is a power of two
+     */
+    bool isPowerOfTwo(int n) {
+        // Write your code here
+        if (n%2==0){
+            isPowerOfTwo(n/2);
+        }else if(n==1){
+                        return true;
+
+            
+        }else {
+            return false;
+        }
+    }
+};
