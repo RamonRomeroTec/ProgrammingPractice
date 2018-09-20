c32df0231297969861a2e257f53ab8e2eb4be6b0
ramonromerotec <a01700318@itesm.mx>
2018-09-20 06:27:10 +0000

    DP!

--- moreTrainning/cutrod.py
+++ moreTrainning/cutrod.py
@@ -1,9 +1,10 @@
 def cutrod(prices, longitud):
 	q=0
+	
 	if longitud==0:
 		return 0
 
-	for i in range(1,longitud):
+	for i in range(1,longitud+1):
 		print(q)
 		q=max(q, prices[i]+ cutrod(prices, longitud-i))
 
@@ -11,5 +12,6 @@ def cutrod(prices, longitud):
 
 
 if __name__ == '__main__':
-	arr=[0,1,5,8,9,10,11]
-	cutrod(arr, 5)
+	arr=[0,2,5,6,9,10,11]
+	
+	print(cutrod(arr, 3))
