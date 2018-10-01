a77a6c78eaee4baad4e328014ffc5af7407f422d
RamonRomeroTec <A01700318@itesm.mx>
2018-10-01 17:58:14 -0500

    more Graphs

--- CrackingTheCodeInterview/2.LinkedLists/2.1.py
+++ CrackingTheCodeInterview/2.LinkedLists/2.1.py
@@ -6,11 +6,11 @@ How would you solve this problem if a temporary buffer is not allowed? Hints: #9
 '''
 class ListNode(object):
     def __init__(self, val, next=None):
-        self.value = val
+        self.val = val
         self.next = next
 
     def __str__(self):
-        return str(self.value)
+        return str(self.val)
 
     def gnext(self):
         return self.next
@@ -18,7 +18,7 @@ class ListNode(object):
 
 def gothru(node):
 
-    print(node.value)
+    print(node.val)
     if node.next==None:
         print("finished")
 
@@ -43,7 +43,7 @@ def gothrudel(node, r):
 '''
 
 def find_next(node, v):
-    if node.next.value!=v:
+    if node.next.val!=v:
         return node.next
     else:
         if node.next==None:
@@ -57,9 +57,9 @@ def deleterepeatedunsorted(node, traveled):
     if node.next==None:
         print("Deleted repeated")
     else:
-        traveled.add(node.value)
-        if node.next.value in traveled: # el siguiente esta repetido
-            node.next=find_next(node, node.next.value)
+        traveled.add(node.val)
+        if node.next.val in traveled: # el siguiente esta repetido
+            node.next=find_next(node, node.next.val)
             if node.next==None:
                 print("Deleted repeateds")
             else:
@@ -71,18 +71,81 @@ def deleterepeatedunsorted(node, traveled):
             #deleteit
 
 def deleterepeatedunsortedNoBuff(node):
-    cur=node.value
+    cur=node.val
     if node.next==None:
         print("Deleted repeated")
     else:
-        if node.next.value in traveled: # el siguiente esta repetido
-            node.next=find_next(node, node.next.value)
+        if node.next.val in traveled: # el siguiente esta repetido
+            node.next=find_next(node, node.next.val)
             if node.next==None:
                 print("Deleted repeateds")
         else:
             deleterepeatedunsorted(node.next, traveled)
 
 
+
+
+
+
+
+
+def aux(A,s,l):
+
+    print(A.val not in s, A.val, s, l)
+    if A.val not in s:
+        s.add(A.val)
+        l.append(A)
+
+
+    if A.next==None:
+        return l
+    else:
+        return aux(A.next,s,l)
+
+def gen(l):
+    if len(l)>1:
+
+        for i in range(len(l)-1):
+            l[i].next=l[i+1]
+
+
+        l[-1].next=None
+        a=
+
+        print(l[-1].val, l[-1].next, "af")
+
+
+        print(l[0].val, l[0].next, "l")
+        print(l[1].val, l[1].next, "lm")
+        print(l[-1].val, l[-1].next, "lm")
+
+        return l[0]
+
+    else:
+        l[0].next=None
+        return l[0]
+
+
+
+class Solution:
+    # @param A : head node of linked list
+    # @return the head node in the linked list
+    def deleteDuplicates(self, A):
+        head = A
+        while A:
+            while A.next and A.next.val == A.val:
+                A.next = A.next.next
+            A = A.next
+        return head
+    '''
+    def deleteDuplicates(self, A):
+        s=set()
+        l=[]
+        a=aux(A, s, l)
+        print(a)
+
+        return gen(a)
+    '''
 
 if __name__ == '__main__':
     '''
@@ -102,3 +165,7 @@ if __name__ == '__main__':
     deleterepeatedunsorted(h2, b)
 
     gothru(h2)
+
+
+    h5=ListNode(19,ListNode(19,ListNode(20,ListNode(20, ListNode(20, ListNode(20, ListNode(20, None)))))))
+    print(gothru(Solution().deleteDuplicates(h5)))
---
+++ CrackingTheCodeInterview/4.TreesAndGraphs/init.py
@@ -0,0 +1,60 @@
+'''
+
+
+
+
+      5
+  2       8
+1   3   6   10
+
+
+
+
+
+
+'''
+
+
+class BinTreeNode(object):
+    """docstring for [object Object]."""
+    def __init__(self, v,l=None, r=None):
+        self.value = v
+        self.left = l
+        self.right = r
+
+
+def inOrderTransversal(node):
+    if node!=None:
+        inOrderTransversal(node.left)
+        print(node.value)
+        inOrderTransversal(node.right)
+
+def preOrderTransversal(node):
+    if node!=None:
+        print(node.value)
+        preOrderTransversal(node.left)
+        preOrderTransversal(node.right)
+
+
+def postOrderTransversal(node):
+    if node!=None:
+        preOrderTransversal(node.left)
+        preOrderTransversal(node.right)
+        print(node.value)
+
+
+
+if __name__ == '__main__':
+    a=BinTreeNode(1)
+    c=BinTreeNode(3)
+    b=BinTreeNode(2,a,c)
+    f=BinTreeNode(6)
+    g=BinTreeNode(10)
+    e=BinTreeNode(8,f,g)
+    d=BinTreeNode(5,b,e)
+    print("InOrder")
+    inOrderTransversal(d)
+    print("PreOrder")
+    preOrderTransversal(d)
+    print("PostOrderTransversal")
+    postOrderTransversal(d)
