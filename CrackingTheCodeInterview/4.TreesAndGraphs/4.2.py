23f3a053ebe2b870c1460338496b84595969893c
RamonRomeroTec <A01700318@itesm.mx>
2018-10-03 22:22:54 -0500

    trees

---
+++ CrackingTheCodeInterview/4.TreesAndGraphs/4.2.py
@@ -0,0 +1,58 @@
+
+
+class Node(object):
+    """docstring for [object Object]."""
+    def __init__(self, val=None, lnodes=None):
+        self.val=val
+        self.nodes=lnodes
+
+
+class Graph(object):
+    def __init__(self, arg):
+        self.nodes=arg
+
+
+
+def findhalf(l):
+    a=len(l)
+    if a%2==0:
+
+    else:
+
+
+
+
+def addtonode(head,node):
+    if node.value<head.value:
+        #si es menor va a la izquerida
+
+    else:
+
+
+        
+    if head.left!=None or head.right!=None:
+
+
+    else:
+
+
+
+
+
+if __name__ == '__main__':
+
+    a=[1,2,3,5,6,8,10]
+'''
+
+
+
+
+      5
+  2       8
+1   3   6   10
+
+
+
+
+
+'''
