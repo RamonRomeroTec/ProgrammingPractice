22bf45454d9caef7cd9d30777e7036629a61c936
RamonRomeroTec <ramonromeroqro@protonmail.com>
2019-02-17 00:23:05 -0600

    more code

--- latest season/peakIndexInMountainArray.py
+++ latest season/peakIndexInMountainArray.py
@@ -1,8 +1,29 @@
+'''
+Note: return A.index(max(A)) -> N Complexity, 
+Aplicar busqueda binaria para reducir la complejidad, basandonos en
+asumir que el arreglo es triangular 
+
+'''
+
+
 class Solution(object):
     def peakIndexInMountainArray(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        return A.index(max(A))
-        
+
+        # return A.index(max(A))
+        f, l = 0, len(A)-1
+        if l+1 < 3:
+            return None
+        if l+1 == 3:
+            return A[1]
+
+        while(1):
+            mid = (l+f)/2
+            # middle
+            if A[mid-1] <= A[mid] and A[mid] >= A[mid+1]:
+                return mid
+            # desc
+            elif A[mid-1] >= A[mid] and A[mid] >= A[mid+1]:
+                l = mid
+            # asc
+            elif A[mid-1] <= A[mid] and A[mid] <= A[mid+1]:
+                f = mid
