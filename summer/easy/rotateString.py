da73f55de11d3c81ebbe89eaeb677988194cb4a8
RamonRomeroTec <rr100@live.com.mx>
2019-08-04 23:37:39 -0500

    another problem

---
+++ summer/easy/rotateString.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        if len(A)>len(B):
+            return False
+        A = A+A
+        return B in A
+        
