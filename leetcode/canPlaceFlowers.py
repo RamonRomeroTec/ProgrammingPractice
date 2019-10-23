4aba56c79a726ae0896e1a833048eee4acadbefa
RamonRomeroTec <rr100@live.com.mx>
2019-10-23 16:45:46 -0500

    10-23-2019//16:45:44 -> Another Problem

---
+++ leetcode/canPlaceFlowers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        i=0
+        c=0
+        while i<len(flowerbed):
+            if (flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+                flowerbed[i] = 1
+                c+=1
+            i+=1
+        return c >= n
