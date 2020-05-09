d83cacced506d72d37bdc30805a5c24d0795fe74
Ramón Romero <rr100@live.com.mx>
2020-05-09 01:00:10 -0500

    code

---
+++ code/destCity.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def destCity(self, paths):
+        d=set()
+        o=set()
+        for p in paths:
+            ori,dest = p
+            d.add(dest)
+            o.add(ori)
+        return list(d-o)[0]
+            
+            
+            
+            
+        
